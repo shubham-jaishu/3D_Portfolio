603c6e13ddd79979e11f1f287c372e4e0527c4af
Shubham Kumar <109282657+shubham-jaishu@users.noreply.github.com>
2025-03-20 11:33:18 +0530

    deploment error resolved

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -26,6 +26,7 @@ export type TProject = {
   }[];
   image: string;
   sourceCodeLink: string;
+  liveDemo: string;
 } & Required<Pick<TCommonProps, "name">>;
 
 export type TTechnology = Required<Omit<TCommonProps, "title">>;
